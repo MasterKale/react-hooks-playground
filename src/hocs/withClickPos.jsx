57eec83ce75256593dc996363773a19eff25cc88
Matthew Miller <matthew@millerti.me>
2019-03-02 15:03:55 -0800

    Create withClickPos HOC

---
+++ src/hocs/withClickPos.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function withClickPos(WrappedComponent) {
+  return class extends React.PureComponent {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        x: 0,
+        y: 0,
+      };
+
+      this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    clickHandler(event) {
+      this.setState({ x: event.x, y: event.y });
+    }
+
+    componentDidMount() {
+      window.addEventListener('click', this.clickHandler);
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('click', this.clickHandler);
+    }
+
+    render() {
+      const {
+        x,
+        y,
+      } = this.state;
+
+      return <WrappedComponent clickPos={{ x, y }} {...this.props} />
+    }
+  }
+};
+
+export default withClickPos;

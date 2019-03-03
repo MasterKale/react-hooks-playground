08455199f8d9fcf122accfbb99e548605f0d2f58
Matthew Miller <matthew@millerti.me>
2019-03-02 22:33:59 -0800

    Import new Budget Tracker App

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+// import App from './hooks-vs-hocs/App';
+import App from './budget-tracker/App';
 
 ReactDOM.render(
   <App />,

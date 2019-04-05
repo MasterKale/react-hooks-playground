3f9d62968ee4f78a0ceb547725a50f0c0f9cb414
Matthew Miller <matthew.miller@bcgdv.com>
2019-04-05 16:00:28 -0700

    Pretty-up the app

--- src/hooks-vs-hocs/App.jsx
+++ src/hooks-vs-hocs/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
-import { AppClass, AppClassHOC } from './AppClass';
+import Card from '../_veneer/card';
+
+import { AppClassHOC } from './AppClass';
 import { AppFunc, AppFunc2 } from './AppFunc';
 
 const App = () => (
   <React.Fragment>
-    <h2>Class Components:</h2>
-    <AppClass />
-    <AppClassHOC />
-    <h2>Function Components:</h2>
-    <AppFunc />
-    <AppFunc2 />
+    <h2>HOC and Class Component</h2>
+    <Card>
+      <AppClassHOC />
+    </Card>
+    <h2>Hook and Function Component</h2>
+    <Card>
+      <AppFunc />
+    </Card>
+    <h2>Hook Reuse</h2>
+    <Card>
+      <AppFunc2 />
+    </Card>
   </React.Fragment>
 );
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.scss';
+
 import App1 from './hooks-vs-hocs/App';
 import App2 from './budget-tracker/App';
 
 const Index = () => (
   <React.Fragment>
-    <h1>Hooks vs HOCs</h1>
-    <App1 />
-    <hr />
-    <h1>Budget Tracker</h1>
-    <App2 />
-    <hr />
+    <div style={{ maxWidth: '700px', margin: 'auto' }}>
+      <h1>HOCs vs Hooks</h1>
+      <App1 />
+      <hr />
+      <h1>Budget Tracker</h1>
+      <App2 />
+      <hr />
+    </div>
   </React.Fragment>
 );
 

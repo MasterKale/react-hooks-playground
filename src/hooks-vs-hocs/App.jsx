8cdca0be516f9a0f6c304354e7e5fd588895a135
Matthew Miller <matthew.miller@bcgdv.com>
2019-04-05 16:04:47 -0700

    Add some clarifying text to titles

--- src/hooks-vs-hocs/App.jsx
+++ src/hooks-vs-hocs/App.jsx
@@ -7,15 +7,15 @@ import { AppFunc, AppFunc2 } from './AppFunc';
 
 const App = () => (
   <React.Fragment>
-    <h2>HOC and Class Component</h2>
+    <h2>HOC and Class Component (AppClass)</h2>
     <Card>
       <AppClassHOC />
     </Card>
-    <h2>Hook and Function Component</h2>
+    <h2>Hook and Function Component (AppFunc)</h2>
     <Card>
       <AppFunc />
     </Card>
-    <h2>Hook Reuse</h2>
+    <h2>Hook Reuse (AppFunc2)</h2>
     <Card>
       <AppFunc2 />
     </Card>
